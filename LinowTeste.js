bd155b7106f890a91c75d07b85439250bc4c65f4
Leonardo Lizzi <leonardolizzirocha@gmail.com>
2026-07-28 20:22:23 -0300

    variaveis

--- LinowTeste.js
+++ LinowTeste.js
@@ -1,2 +1,5 @@
-const linow = require('linow');
-const batata
+    let nomeEvento = "Linow E-sports";
+    let nomeCidadeEvento = "Foz do iguaçu";
+    let anoEvento = "2026";
+
+    let equipesEvento = ['Binary Masters', 'Code Warriors', 'Bug Hunters', 'Stack Legends', 'Null Squad'];
