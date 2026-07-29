29177315bcfd5a79bef2591403db50ae456b9102
Leonardo Lizzi <leonardolizzirocha@gmail.com>
2026-07-28 21:40:24 -0300

    falta so a parte 5

--- LinowTeste.js
+++ LinowTeste.js
@@ -11,8 +11,30 @@
     console.log(`Primeira equipe: ` + (equipesEvento[0]));
     console.log(`Última equipe: ` + (equipesEvento[4]));
 
-    console.log(`A equipe ${equipesEvento[0]} possui ${pontuacaoEquipe[0]} pontos.`)
-    console.log(`A equipe ${equipesEvento[1]} possui ${pontuacaoEquipe[1]} pontos.`)
-    console.log(`A equipe ${equipesEvento[2]} possui ${pontuacaoEquipe[2]} pontos.`)
-    console.log(`A equipe ${equipesEvento[3]} possui ${pontuacaoEquipe[3]} pontos.`)
-    console.log(`A equipe ${equipesEvento[4]} possui ${pontuacaoEquipe[4]} pontos.`)
+    console.log(`A equipe ${equipesEvento[0]} possui ${pontuacaoEquipe[0]} pontos.`);
+    console.log(`A equipe ${equipesEvento[1]} possui ${pontuacaoEquipe[1]} pontos.`);
+    console.log(`A equipe ${equipesEvento[2]} possui ${pontuacaoEquipe[2]} pontos.`);
+    console.log(`A equipe ${equipesEvento[3]} possui ${pontuacaoEquipe[3]} pontos.`);
+    console.log(`A equipe ${equipesEvento[4]} possui ${pontuacaoEquipe[4]} pontos.`);
+
+    console.log(
+`=================================
+
+RELATÓRIO DO TORNEIO
+
+=================================`);
+
+console.log(`Equipe: ${equipesEvento[0]} 
+pontos: ${pontuacaoEquipe[0]}\n`);
+
+console.log(`Equipe: ${equipesEvento[1]} 
+pontos: ${pontuacaoEquipe[1]}\n`);
+
+console.log(`Equipe: ${equipesEvento[2]} 
+pontos: ${pontuacaoEquipe[2]}\n`);
+
+console.log(`Equipe: ${equipesEvento[3]} 
+pontos: ${pontuacaoEquipe[3]}\n`);
+
+console.log(`Equipe: ${equipesEvento[4]} 
+pontos: ${pontuacaoEquipe[4]}\n`);
