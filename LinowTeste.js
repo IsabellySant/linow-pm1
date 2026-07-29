f4e7716dfe9e5d69baeef8d37a1d09b6907cc4db
Leonardo <leonardolizzirocha@gmail.com>
2026-07-29 02:49:28 -0300

    acho q ta pronto

--- LinowTeste.js
+++ LinowTeste.js
@@ -3,38 +3,58 @@
     let anoEvento = "2026";
 
     const equipesEvento = ['Binary Masters', 'Code Warriors', 'Bug Hunters', 'Stack Legends', 'Null Squad'];
-    const pontuacaoEquipe = ['10', '25', '15', '30', '60'];
+    const pontuacaoEquipe = [10, 25, 15, 30, 60];
+    let somaPontuacao = 0;
+    let mediaPontuacao = 0;
+    let maiorPontuacao = 0;
 
-    console.log(`A ${nomeEvento} acontecerá em ${nomeCidadeEvento} no ano de ${anoEvento}.`)
+    console.log(`A ${nomeEvento} acontecerá em ${nomeCidadeEvento} no ano de ${anoEvento}.`);
 
-    console.log(`Total de equipes no evento: ` + (equipesEvento.length));
-    console.log(`Primeira equipe: ` + (equipesEvento[0]));
-    console.log(`Última equipe: ` + (equipesEvento[4]));
+    console.log(`\nTotal de equipes no evento: ${equipesEvento.length}`);
+    console.log(`Primeira equipe: ${equipesEvento[0]}`);
+    console.log(`Última equipe: ${equipesEvento[4]}`);
 
-    console.log(`A equipe ${equipesEvento[0]} possui ${pontuacaoEquipe[0]} pontos.`);
-    console.log(`A equipe ${equipesEvento[1]} possui ${pontuacaoEquipe[1]} pontos.`);
-    console.log(`A equipe ${equipesEvento[2]} possui ${pontuacaoEquipe[2]} pontos.`);
-    console.log(`A equipe ${equipesEvento[3]} possui ${pontuacaoEquipe[3]} pontos.`);
-    console.log(`A equipe ${equipesEvento[4]} possui ${pontuacaoEquipe[4]} pontos.`);
+    for (let i = 0; i < equipesEvento.length; i++) {
+        console.log(`\nA equipe ${equipesEvento[i]} possui ${pontuacaoEquipe[i]} pontos.`);
+    }
 
-    console.log(
-`=================================
+console.log(`
+=================================
 
 RELATÓRIO DO TORNEIO
 
-=================================`);
+=================================\n`);
 
-console.log(`Equipe: ${equipesEvento[0]} 
-pontos: ${pontuacaoEquipe[0]}\n`);
+for (let i = 0; i < equipesEvento.length; i++) {
+    console.log(`Equipe: ${equipesEvento[i]} 
+pontos: ${pontuacaoEquipe[i]}\n`);
+}
 
-console.log(`Equipe: ${equipesEvento[1]} 
-pontos: ${pontuacaoEquipe[1]}\n`);
+function calcularSoma() {
+    somaPontuacao = 0;
+    for (let i = 0; i < pontuacaoEquipe.length; i++) {
+        somaPontuacao += pontuacaoEquipe[i];
+    }
+}
 
-console.log(`Equipe: ${equipesEvento[2]} 
-pontos: ${pontuacaoEquipe[2]}\n`);
+function calcularMedia() {
+    mediaPontuacao = somaPontuacao / pontuacaoEquipe.length;
+}
 
-console.log(`Equipe: ${equipesEvento[3]} 
-pontos: ${pontuacaoEquipe[3]}\n`);
+function encontrarMaiorPontuacao() {
+    maiorPontuacao = pontuacaoEquipe[0];
+    for (let i = 1; i < pontuacaoEquipe.length; i++) {
+        if (pontuacaoEquipe[i] > maiorPontuacao) {
+            maiorPontuacao = pontuacaoEquipe[i];
+        }
+    }
+}
 
-console.log(`Equipe: ${equipesEvento[4]} 
-pontos: ${pontuacaoEquipe[4]}\n`);
+calcularSoma();
+console.log(`\nSoma total de pontos: ${somaPontuacao}`);
+
+calcularMedia();
+console.log(`Média de pontos: ${mediaPontuacao}`);
+
+encontrarMaiorPontuacao();
+console.log(`Maior pontuação: ${maiorPontuacao}`);
